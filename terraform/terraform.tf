98714d368903e10c56a5f62d05af354ea5aded9b
PrasadReddy-V <prasad.dreambig@gmail.com>
2026-06-11 11:25:24 +0530

    updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "devopsdock-terraform-backend-bucket-196"
+    key    = "s3-backend"
+    region = "ap-south-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
